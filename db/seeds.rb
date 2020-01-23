6eab10e63071f76ca5cf3fe20b61db043d11faaf
Rails Udemy Course <justin.paro@gmail.com>
2020-01-23 01:57:48 -0600

    added user and message model resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Katie", password: "password")
+User.create(username: "Bob", password: "password")
+User.create(username: "Phoenix", password: "password")
+User.create(username: "Shannon", password: "password")
+User.create(username: "Dan", password: "password")
