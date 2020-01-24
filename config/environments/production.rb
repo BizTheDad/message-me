d08872edcb1ef5b30742133eef34f7a048aea9a7
Rails Udemy Course <justin.paro@gmail.com>
2020-01-24 09:46:00 -0600

    updating production config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.web_socket_server_url = "wss://message-me-jparo.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://action-cable-example.herokuapp.com', 'http://action-cable-example.herokuapp.com']
 end
