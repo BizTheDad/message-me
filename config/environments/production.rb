5d68c83819fe3fe4cbc87d66d4ac6df812df556a
Rails Udemy Course <justin.paro@gmail.com>
2020-01-24 17:26:45 -0600

    modified the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.web_socket_server_url = "wss://message-me-jparo.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://action-cable-example.herokuapp.com', 'http://action-cable-example.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://message-me-jparo.herokuapp.com', 'http://message-me-jparo.herokuapp.com']
 end
